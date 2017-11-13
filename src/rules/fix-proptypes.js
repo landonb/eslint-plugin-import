47d91b321da87e2100d621838da0213fba879115
landonb <landonb@users.noreply.github.com>
2017-11-13 08:50:26 -0600

    feat: New rule: Replace React.PropTypes with new prop-types package.

---
+++ src/rules/fix-proptypes.js
@@ -0,0 +1,334 @@
+'use strict'
+
+import importType from '../core/importType'
+import isStaticRequire from '../core/staticRequire'
+
+const util = require('util');
+
+const debug = require('debug')('eslint-plugin-import:order');
+
+const defaultGroups = ['builtin', 'external', 'parent', 'sibling', 'index']
+
+// REPORTING
+
+function reverse(array) {
+  return array.map(function (v) {
+    return {
+      name: v.name,
+      rank: -v.rank,
+      node: v.node,
+    }
+  }).reverse()
+}
+
+function findOutOfOrder(imported) {
+  if (imported.length === 0) {
+    return []
+  }
+  let maxSeenRankNode = imported[0]
+  return imported.filter(function (importedModule) {
+    const res = importedModule.rank < maxSeenRankNode.rank
+    if (maxSeenRankNode.rank < importedModule.rank) {
+      maxSeenRankNode = importedModule
+    }
+    return res
+  })
+}
+
+function reportOutOfOrder(context, imported, outOfOrder, order) {
+  outOfOrder.forEach(function (imp) {
+    const found = imported.find(function hasHigherRank(importedItem) {
+      return importedItem.rank > imp.rank
+    })
+    context.report(imp.node, '`' + imp.name + '` import should occur ' + order +
+      ' import of `' + found.name + '`')
+  })
+}
+
+function makeOutOfOrderReport(context, imported) {
+  const outOfOrder = findOutOfOrder(imported)
+  if (!outOfOrder.length) {
+    return
+  }
+  // There are things to report. Try to minimize the number of reported errors.
+  const reversedImported = reverse(imported)
+  const reversedOrder = findOutOfOrder(reversedImported)
+  if (reversedOrder.length < outOfOrder.length) {
+    reportOutOfOrder(context, reversedImported, reversedOrder, 'after')
+    return
+  }
+  reportOutOfOrder(context, imported, outOfOrder, 'before')
+}
+
+// DETECTING
+
+function computeRank(context, ranks, name, type) {
+  return ranks[importType(name, context)] +
+    (type === 'import' ? 0 : 100)
+}
+
+function registerNode(context, node, name, type, ranks, imported) {
+  const rank = computeRank(context, ranks, name, type)
+  if (rank !== -1) {
+    imported.push({name, rank, node})
+  }
+}
+
+function isInVariableDeclarator(node) {
+  return node &&
+    (node.type === 'VariableDeclarator' || isInVariableDeclarator(node.parent))
+}
+
+const types = ['builtin', 'external', 'internal', 'parent', 'sibling', 'index']
+
+// Creates an object with type-rank pairs.
+// Example: { index: 0, sibling: 1, parent: 1, external: 1, builtin: 2, internal: 2 }
+// Will throw an error if it contains a type that does not exist, or has a duplicate
+function convertGroupsToRanks(groups) {
+  const rankObject = groups.reduce(function(res, group, index) {
+    if (typeof group === 'string') {
+      group = [group]
+    }
+    group.forEach(function(groupItem) {
+      if (types.indexOf(groupItem) === -1) {
+        throw new Error('Incorrect configuration of the rule: Unknown type `' +
+          JSON.stringify(groupItem) + '`')
+      }
+      if (res[groupItem] !== undefined) {
+        throw new Error('Incorrect configuration of the rule: `' + groupItem + '` is duplicated')
+      }
+      res[groupItem] = index
+    })
+    return res
+  }, {})
+
+  const omittedTypes = types.filter(function(type) {
+    return rankObject[type] === undefined
+  })
+
+  return omittedTypes.reduce(function(res, type) {
+    res[type] = groups.length
+    return res
+  }, rankObject)
+}
+
+function makeNewlinesBetweenReport (context, imported, newlinesBetweenImports) {
+  const getNumberOfEmptyLinesBetween = (currentImport, previousImport) => {
+    const linesBetweenImports = context.getSourceCode().lines.slice(
+      previousImport.node.loc.end.line,
+      currentImport.node.loc.start.line - 1
+    )
+
+    return linesBetweenImports.filter((line) => !line.trim().length).length
+  }
+  let previousImport = imported[0]
+
+  imported.slice(1).forEach(function(currentImport) {
+    const emptyLinesBetween = getNumberOfEmptyLinesBetween(currentImport, previousImport)
+
+    if (newlinesBetweenImports === 'always'
+        || newlinesBetweenImports === 'always-and-inside-groups') {
+      if (currentImport.rank !== previousImport.rank && emptyLinesBetween === 0)
+      {
+        context.report(
+          previousImport.node, 'There should be at least one empty line between import groups'
+        )
+      } else if (currentImport.rank === previousImport.rank
+        && emptyLinesBetween > 0
+        && newlinesBetweenImports !== 'always-and-inside-groups')
+      {
+        context.report(
+          previousImport.node, 'There should be no empty line within import group'
+        )
+      }
+    } else {
+      if (emptyLinesBetween > 0) {
+        context.report(previousImport.node, 'There should be no empty line between import groups')
+      }
+    }
+
+    previousImport = currentImport
+  })
+}
+
+function makePropTypesReport(context, propTypeNodes, imported) {
+  const code = context.getSourceCode();
+  propTypeNodes.forEach(function(propNode) {
+    context.report({
+      node: propNode,
+      // prop-types package first commit: April 7, 2017.
+      message: 'React 16 uses PropTypes from prop-types package.',
+      fix: fixer => {
+        const fixed = code.getText(propNode).replace('React.PropTypes', 'PropTypes');
+        debug('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+        return fixer.replaceText(propNode, fixed);
+      },
+    });
+  });
+
+  const lastImportNode = imported[imported.length-1].node;
+  context.report({
+    node: propTypeNodes[0],
+    // NOTE (landonb): The message is only shown if not --fix. I.e., fix runs silently.
+    message: 'Transitioning to prop-types package needs additional import.',
+    fix: fixer => {
+debug('TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT');
+      return fixer.insertTextAfter(lastImportNode, `\nimport PropTypes from 'prop-types';`);
+    },
+  });
+}
+
+function registerPropTypes(node, propTypeNodes) {
+  // We're looking for, e.g.,
+  // MyComponent.propTypeNodes = {
+  //   myProperty: React.PropTypes.string,
+  //   ...
+  node.properties.forEach(function (propNode) {
+    // 2017-11-12 (landonb): I'm probably doing this wrong. But it works.
+    if (propNode.value.type == 'MemberExpression') {
+      if (propNode.value.object.name === undefined) {
+        try {
+          const objName = propNode.value.object.object.name;
+          const prpName = propNode.value.object.property.name;
+          if ((objName == 'React') && (prpName == 'PropTypes')) {
+if (true) { debugger; }
+            //console.log(`TMI: ${util.inspect(propNode, {showHidden: true, depth: null})}`);
+            propTypeNodes.push(propNode);
+          }
+        }
+        catch (err) {
+          console.warn(`Unexpected: No name(s): ${util.inspect(propNode)}\n\n`);
+        }
+      }
+      else if (propNode.value.object.name != 'PropTypes') {
+        console.warn(`Unexpected: Not PropTypes: ${util.inspect(propNode)}\n\n`);
+      }
+    }
+  });
+}
+
+module.exports = {
+  meta: {
+    docs: {},
+
+    schema: [
+      {
+        type: 'object',
+        properties: {
+          groups: {
+            type: 'array',
+          },
+          'newlines-between': {
+            enum: [
+              'ignore',
+              'always',
+              'always-and-inside-groups',
+              'never',
+            ],
+          },
+        },
+        additionalProperties: false,
+      },
+    ],
+
+    fixable: 'whitespace',
+  },
+
+  create: function importOrderRule (context) {
+    const options = context.options[0] || {}
+    const newlinesBetweenImports = options['newlines-between'] || 'ignore'
+    let ranks
+
+    try {
+      ranks = convertGroupsToRanks(options.groups || defaultGroups)
+    } catch (error) {
+      // Malformed configuration
+      return {
+        Program: function(node) {
+          context.report(node, error.message)
+        },
+      }
+    }
+    let imported = []
+    let level = 0
+
+    let propTypeNodes = [];
+
+    function incrementLevel() {
+      level++
+    }
+    function decrementLevel() {
+      level--
+    }
+
+    return {
+      // Respond to AST parsing.
+      //
+      // To learn by exploring, hook all events, e.g.,:
+      //
+      //  const util = require('util');
+      //  ...
+      //  '*': function handleAnything(node) {
+      //    console.log(util.inspect(node, {showHidden: false, depth: null}))
+      //    // or, tl;dr:
+      //    console.log(
+      //      `at node: ${node.type} for ${node.name} at ` +
+      //      `${node.loc.start.line}:${node.loc.start.column} ` +
+      //      `to ${node.loc.end.line}:${node.loc.end.column}`
+      //    );
+      //  }
+
+      ObjectExpression: function handleObjectExpression(node) {
+        if (node.properties == null) {
+          console.warn(`Unexpected: node.properties is null: ${util.inspect(node)}`);
+        }
+        else if (node.properties.length == 0) {
+          console.log(`Unexpected?: node.properties is empty: ${util.inspect(node)}`);
+        }
+        else {
+          registerPropTypes(node, propTypeNodes);
+        }
+        incrementLevel()
+      },
+
+      ImportDeclaration: function handleImports(node) {
+        if (node.specifiers.length) { // Ignoring unassigned imports
+          const name = node.source.value
+          registerNode(context, node, name, 'import', ranks, imported)
+        }
+      },
+
+      CallExpression: function handleRequires(node) {
+        if (level !== 0 || !isStaticRequire(node) || !isInVariableDeclarator(node.parent)) {
+          return
+        }
+        const name = node.arguments[0].value
+        registerNode(context, node, name, 'require', ranks, imported)
+      },
+
+      'Program:exit': function reportAndReset() {
+        if (propTypeNodes.length > 0) {
+          makePropTypesReport(context, propTypeNodes, imported);
+        }
+
+        makeOutOfOrderReport(context, imported)
+
+        if (newlinesBetweenImports !== 'ignore') {
+          makeNewlinesBetweenReport(context, imported, newlinesBetweenImports)
+        }
+
+        imported = []
+      },
+
+      FunctionDeclaration: incrementLevel,
+      FunctionExpression: incrementLevel,
+      ArrowFunctionExpression: incrementLevel,
+      BlockStatement: incrementLevel,
+      'FunctionDeclaration:exit': decrementLevel,
+      'FunctionExpression:exit': decrementLevel,
+      'ArrowFunctionExpression:exit': decrementLevel,
+      'BlockStatement:exit': decrementLevel,
+      'ObjectExpression:exit': decrementLevel,
+    }
+  },
+}
